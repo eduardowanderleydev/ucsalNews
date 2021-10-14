b1f433200aa701b6aae749c7b9fe1b44aef7ce8d
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2021-10-14 18:36:18 -0300

    Initial commit

---
+++ backend/src/main/java/br/ucsal/ucsalnews/UcsalNewsApplication.java
@@ -0,0 +1,13 @@
+package br.ucsal.ucsalnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcsalNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcsalNewsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/ucsal/ucsalnews/UcsalNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucsal.ucsalnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcsalNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
