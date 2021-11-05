a841025207e959a7586bbb06765d7de9fa32aa27
Guilherme Falcão <guifalcao2010@hotmail.com>
2021-11-05 16:43:53 -0300

    feat: added input form validations with react hook form

--- frontend/ucsal_news/src/components/Pages/SignUp/presentational.jsx
+++ frontend/ucsal_news/src/components/Pages/SignUp/presentational.jsx
@@ -1,37 +1,69 @@
 import React from 'react'
 import './styles.css'
-import {useHistory} from 'react-router-dom'
+
+//External Libraries
+import {useHistory} from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const validationForm = yup.object().shape({
+    email: yup.string().required("Insira seu Email").email("Necessário um email válido"),
+    username: yup.string().required("Insira seu Username").max(20, "O seu nome de usuário precisa ter menos que 20 caracteres"),
+    password: yup.string().required("Insira uma Senha ").min(6, "A senha precisa de no mínimo 6 dígitos"),
+    confirmPassword: yup.string().required("Insira uma Senha").min(6, "A senha precisa de no mínimo 6 dígitos"),
+})
 
 function Signup() {
 
     const history = useHistory();
 
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(validationForm)
+    })
+    const onsubmit = data => console.log(data)
+    
     return(
         <div>
-        <div className="container">
-        <h1>Sign Up</h1>
-        <p>Please fill in this form to create an account.</p>
-        <hr/>
+            <div className="container">
+            <h1>Sign Up</h1>
+            <p>Please fill in this form to create an account.</p>
+            <hr/>
 
-        <label for="email"><b>Email</b></label>
-        <input 
-        type="text" 
-        placeholder="Enter Email" 
-        name="email" />
+        <form onSubmit={handleSubmit(onsubmit)}>
 
-        <label for="email"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required/>
-
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required/>
-
-        <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" required/>
-
-        <div className="clearfix">
-        <button type="button" className="cancelbtn">Cancel</button>
-        <button type="submit" className="signupbtn"onClick={() => {history.push('/')}} >Sign Up</button>
+        <div>
+            <label><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="email" {...register("email")} />
+            <p className="error-message">{errors.email?.message}</p>
         </div>
+
+        <div>
+            <label><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="username" {...register("username")} />
+            <p className="error-message">{errors.username?.message}</p>
+        </div>
+        
+        <div>
+            <label><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password" {...register("password")} />
+            <p className="error-message">{errors.password?.message}</p>
+        </div>
+       
+        <div>
+            <label><b>Repeat Password</b></label>
+            <input type="password" placeholder="Repeat Password" name="confirmPassword" {...register("confirmPassword")} />
+            <p className="error-message">{errors.confirmPassword?.message}</p>
+        </div>
+        
+        <div>
+        <button type="button" className="cancelbtn">Cancel</button>
+        <button 
+        type="submit" 
+        className="signupbtn">Sign Up</button>
+        </div>
+
+        </form>    
     </div>
         </div>
     )
