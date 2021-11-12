dc4da003631e0765fbfeb92c4494d5f2fead0935
Guilherme Falcão <guifalcao2010@hotmail.com>
2021-11-12 18:36:18 -0300

    feat: implemented renderization of news

--- frontend/ucsal_news/src/components/Pages/Home/MainContent/TodoList/TodoList.js
+++ frontend/ucsal_news/src/components/Pages/Home/MainContent/TodoList/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import CreateTask from "../../../../modals/CreateTask";
 
@@ -6,24 +6,57 @@ import "./todoList.css";
 
 function TodoList() {
   const [modal, setModal] = useState(false);
+  const [taskList, setTaskList] = useState([]);
+
+  useEffect(() => {
+    let arr = localStorage.getItem("taskList");
+
+    if (arr) {
+      let obj = JSON.parse(arr);
+      setTaskList(obj);
+    }
+  }, []);
 
   const toggle = () => {
     setModal(!modal);
+  };
+
+  const saveTask = (taskObj) => {
+    let tempList = taskList;
+    tempList.push(taskObj);
+    setTaskList(tempList);
+    setModal(false);
   };
 
   return (
     <>
       <div className="todolist_container">
         <div className="todolist_content">
-          <h3 className="todolist__title">Todo List</h3>
-          <button className="todolist_button" onClick={() => setModal(true)}>
-            Create News
-          </button>
-          <CreateTask toggle={toggle} modal={modal} />
+          <div>
+            <h3 className="todolist__title">UCSAL NEW</h3>
+            <button className="todolist_button" onClick={() => setModal(true)}>
+              Create News
+            </button>
+          </div>
+          <CreateTask toggle={toggle} modal={modal} save={saveTask} />
         </div>
-        
+        <div className="post_container">
+          {taskList.map((item) => (
+            <div className="card">
+              <div className="post_content">
+                <ul>
+                  <li className="card_title">{item.Name}</li>
+                  <div class="cart_bar">
+                    <div class="emptybar"></div>
+                    <div class="filledbar"></div>
+                  </div>
+                  <li className="card_description">{item.Description}</li>
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-     
     </>
   );
 }
--- frontend/ucsal_news/src/components/modals/CreateTask.js
+++ frontend/ucsal_news/src/components/modals/CreateTask.js
@@ -1,25 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
-import './createTask.css'
+import "./createTask.css";
 
-export function CreateTask({ modal, toggle }) {
+export function CreateTask({ modal, toggle, save }) {
+  const [titleNews, settitleNews] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "titleNews") {
+      settitleNews(value);
+    } else {
+      setDescription(value);
+    }
+  };
+
+  const handleSave = () => {
+    let taskObj = {}
+    taskObj['Name'] = titleNews
+    taskObj['Description'] = description
+    save(taskObj)
+  }
+
   return (
     <Modal isOpen={modal} toggle={toggle} className="modal_container">
       <ModalHeader toggle={toggle}>Create News</ModalHeader>
       <ModalBody>
         <form>
-          <input type ="text">
-          </input>
+          <div className="form-group">
+            <label className="label_task_form">Title</label>
+            <input
+              className="form-control"
+              type="text"
+              value={titleNews}
+              name="titleNews"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label className="label_task_form">Description</label>
+            <textarea
+              rows="5"
+              className="form-control"
+              value={description}
+              name="descriptionNews"
+              onChange={handleChange}
+            ></textarea>
+          </div>
         </form>
       </ModalBody>
       <ModalFooter>
         <Button color="secondary" onClick={toggle}>
           Cancel
-        </Button> {' '}
-        <Button color="primary" onClick={toggle}>
+        </Button>{" "}
+        <Button color="primary" onClick={handleSave}>
           Create
-        </Button> 
+        </Button>
       </ModalFooter>
     </Modal>
   );
