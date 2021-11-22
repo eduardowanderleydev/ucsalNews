15688c3d85b6f8e402f83936d5dc75d9d57e6927
Caio Victor Batista de Macedo <79578694+ccaiomacedo@users.noreply.github.com>
2021-11-22 13:54:21 -0300

    Feat: Implementing cors

--- backend/src/main/java/br/ucsal/ucsalnews/UcsalNewsApplication.java
+++ backend/src/main/java/br/ucsal/ucsalnews/UcsalNewsApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableWebMvc
 public class UcsalNewsApplication {
-
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry){
+		registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(UcsalNewsApplication.class, args);
 	}
