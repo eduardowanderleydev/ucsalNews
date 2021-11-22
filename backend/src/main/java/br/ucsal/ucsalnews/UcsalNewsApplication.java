033968ffbc84704775aad8311a7d01380bb7addf
Caio Victor Batista de Macedo <79578694+ccaiomacedo@users.noreply.github.com>
2021-11-22 14:11:38 -0300

    Feat: Implementing imports

--- backend/src/main/java/br/ucsal/ucsalnews/UcsalNewsApplication.java
+++ backend/src/main/java/br/ucsal/ucsalnews/UcsalNewsApplication.java
@@ -2,10 +2,13 @@ package br.ucsal.ucsalnews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableWebMvc
-public class UcsalNewsApplication {
+public class UcsalNewsApplication implements WebMvcConfigurer {
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry){
