acf7a075e30347ae0529b566538b4391fdc71cc1
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2021-11-22 21:12:22 -0300

    refactor : changing CORS configuration.

--- backend/src/main/java/br/ucsal/ucsalnews/controller/CommentController.java
+++ backend/src/main/java/br/ucsal/ucsalnews/controller/CommentController.java
@@ -15,6 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/comment")
+@CrossOrigin
 public class CommentController {
 
     @Autowired
--- backend/src/main/java/br/ucsal/ucsalnews/controller/UserController.java
+++ backend/src/main/java/br/ucsal/ucsalnews/controller/UserController.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
 
     @Autowired
