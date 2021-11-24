c96d7054ed8cfa3c891c04008770aa822368801f
GuilhermePontes01 <guilhermepontesmtm@outlook.com>
2021-11-24 07:43:32 -0300

    Encrypt password SpringSecurity

--- backend/src/main/java/br/ucsal/ucsalnews/UcsalNewsApplication.java
+++ backend/src/main/java/br/ucsal/ucsalnews/UcsalNewsApplication.java
@@ -2,8 +2,9 @@ package br.ucsal.ucsalnews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class UcsalNewsApplication {
 
 	public static void main(String[] args) {
