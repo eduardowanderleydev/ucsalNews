ff0724f3699119062ebff6a2bd512d79716b851b
Guilherme Falcão <guifalcao2010@hotmail.com>
2021-12-13 14:13:36 -0300

    feat: added feature to delete and edit news

---
+++ frontend/ucsal_news/src/components/Card/presentational.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+import "./styles.css";
+
+import EditTask from "../modals/EditTask";
+
+import { FaTrashAlt } from "react-icons/fa";
+import { AiFillEdit } from "react-icons/ai";
+
+function card({ taskObj, index, deleteTask, updateListArray }) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [modal, setModal] = useState(false);
+
+  const today = new Date();
+
+  const date =
+    today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+
+  const handleDelete = () => {
+    deleteTask(index);
+  };
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  const updateTask = (obj) => {
+      updateListArray(obj, index)
+  };
+
+  return (
+    <div>
+      <div className="card_content">
+        <div className="post_content">
+          <ul className="card_info_container">
+            <li className="card_title">{taskObj.Name}</li>
+            <li className="card_description">{taskObj.Description}</li>
+          </ul>
+          <div className="card_date_info">
+            <div className="card_date_info_position">{`${date}`}</div>
+          </div>
+        </div>
+        <div className="icons_container">
+          <div className="icons_area">
+            <AiFillEdit size={25} onClick={() => setModal(true)} />
+            <FaTrashAlt size={20} onClick={handleDelete} />
+          </div>
+        </div>
+      </div>
+      <EditTask modal={modal} toggle={toggle} updateTask={updateTask} taskObj={taskObj} />
+    </div>
+  );
+}
+
+export default card;
---
+++ frontend/ucsal_news/src/components/modals/EditTask.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+import "./createTask.css";
+
+export function EditTask({ modal, toggle, updateTask, taskObj }) {
+  const [titleNews, settitleNews] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "titleNews") {
+      settitleNews(value);
+    } else {
+      setDescription(value);
+    }
+  };
+
+  useEffect(() => {
+    settitleNews(taskObj.Name)
+    setDescription(taskObj.Description)
+  },[])
+
+  const handleUpdate = (e) => {
+      e.preventDefault();
+      let tempObj = {};
+      tempObj['Name'] = titleNews
+      tempObj['Description'] = description
+      updateTask(tempObj)
+  }
+
+  return (
+    <Modal isOpen={modal} toggle={toggle} className="modal_container">
+      <ModalHeader toggle={toggle}>Edit News</ModalHeader>
+      <ModalBody>
+        <form>
+          <div className="form-group">
+            <label className="label_task_form">Title</label>
+            <input
+              className="form-control"
+              type="text"
+              value={titleNews}
+              name="titleNews"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label className="label_task_form">Description</label>
+            <textarea
+              rows="5"
+              className="form-control"
+              value={description}
+              name="descriptionNews"
+              onChange={handleChange}
+            ></textarea>
+          </div>
+        </form>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="secondary" onClick={toggle}>
+          Cancel
+        </Button>{" "}
+        <Button color="primary" onClick={handleUpdate}>
+          Edit
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+}
+
+export default EditTask;
